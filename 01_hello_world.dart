a2a277d0d535c678dd3068d37ceabf260c62b5f8
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-07 15:11:34 -0500

    first commit - add hello

---
+++ 01_hello_world.dart
@@ -0,0 +1,11 @@
+main () {
+
+  //var myName = 'Lilia;
+  // late final myName;
+  //String myName = 'Lilia';
+  const myName = 'lilia';
+  
+  print('hola $myName');
+  print('hola ${myName.toLowerCase()}');
+  print('hola ${1+1}');
+}
