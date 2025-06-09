166811443056d45fd2c6585d9613b75c718406da
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-09 08:41:22 -0500

    Feature set and get

---
+++ 08_getters_setters.dart
@@ -0,0 +1,38 @@
+void main () {
+
+  final  mySquare = Square(side: -5);
+
+  mySquare.side = 5;
+
+  print('area: ${mySquare.calculateArea()}');
+   print('area: ${mySquare.area}');
+
+
+}
+
+class Square {
+
+  double _side;
+
+  Square({ required double side})
+    : _side = side;
+
+  //getter
+
+  double get area {
+    return _side * _side;
+  }
+
+  //setter
+  set side (double value) {
+    print('Setting new value $value');
+    if(value<0) throw 'Value must be >=0';
+
+    _side = value; 
+  }
+
+
+  double calculateArea() {
+    return _side * _side;
+  }
+}
