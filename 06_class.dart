622f0aed5373015dbd83b423e019bcff80fc9647
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-09 08:12:08 -0500

    add classes

--- 06_class.dart
+++ 06_class.dart
@@ -1,6 +1,11 @@
 void main () {
 
-  final Hero wonverine =  Hero('logan', 'regeneracion');
+  // final Hero wonverine =  Hero('logan', 'regeneracion');
+
+  // print(wonverine);
+  // print(wonverine.name);
+
+  final Hero wonverine =  Hero(name:'logan', power:'regeneracion');
 
   print(wonverine);
   print(wonverine.name);
@@ -11,10 +16,22 @@ class Hero {
   String name;
   String power;
 
-  Hero (this.name, this.power);
+  // Hero (this.name, this.power);
 
   // Hero (String pName, String pPower) 
   // : name = pName,
   //   power = pPower; inicializacion controlada
+
+  Hero ({
+    required this.name, 
+    this.power = 'sin poder',
+  });
+
+  @override
+  String toString() {
+    return '$name - $power';
+  }
 }
+
+
 
